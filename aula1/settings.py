1161ae360625c8f432923befeb243da7e0ea3933
Luis Gustavo <piaizete123@gmail.com>
2023-02-27 21:37:54 -0300

    create base_templates and app templates

--- aula1/settings.py
+++ aula1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2=nj^l-c&4lmzj0%-#j!m&_jog4bp5fp3qdt^(f6phxm!6vf7_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list = []
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recipes',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'aula1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base_templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('HOME')
+    return render(request, 'global/home.html')
 
 
 def contato(request):
